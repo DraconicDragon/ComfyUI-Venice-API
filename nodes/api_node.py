4994808308621aa9b71b494adb9dc815e7bc031f
Drac <altdragon@outlook.de>
2025-01-11 17:27:49 +0100

    automatic model list update

--- nodes/api_node.py
+++ nodes/api_node.py
@@ -2,7 +2,6 @@ import base64
 import configparser
 import io
 import os
-from enum import Enum
 from urllib.parse import urljoin
 
 import numpy as np
@@ -12,18 +11,10 @@ from PIL import Image
 from tenacity import retry, stop_after_attempt, wait_exponential
 
 API_ENDPOINTS = {
+    "list_models": "/models",  # response type is list of strings
     "image_generate": "/image/generate",  # response type is dict with images key, each item is image in base64
     "upscale_image": "/image/upscale",  # NOTE: not supported yet in code, response type is image/png file
 }
-
-
-class Status(Enum):
-    TASK_NOT_FOUND = "Task not found"
-    PENDING = "pending"
-    REQUEST_MODERATED = "Request Moderated"
-    CONTENT_MODERATED = "Content Moderated"
-    READY = "completed"
-    ERROR = "error"
 
 
 class ConfigLoader:
@@ -61,10 +52,25 @@ class ConfigLoader:
 config_loader = ConfigLoader()
 
 
-class BaseFlux:
+class GenerateImageBase:
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "generate_image"
     CATEGORY = "venice.ai"
+
+    def get_models(self, type: str) -> list:  # type = image or text model
+        try:
+            headers = {"Authorization": f"Bearer {os.getenv('VENICE_API_KEY')}"}
+            url = urljoin(os.getenv("VENICE_BASE_URL"), API_ENDPOINTS["list_models"])
+            response = requests.request("GET", url, headers=headers).json()
+            # response returns a dict with a data key containing a list of models where 'id' is the model name and what we want with "type" denoting model type
+            return [model["id"] for model in response["data"] if model["type"] == type]
+
+        except Exception as e:
+            print(f"Could not fetch models: {str(e)} \n Attempting to switch to default selection...")
+            if type == "image":
+                return ["flux-dev", "flux-dev-uncensored", "fluently-xl", "pony-realism"]
+            elif type == "text":
+                return ["dolphin-2.9.2-qwen2-72b"]  # dont know which models supported or if any
 
     def process_result(self, result):
         try:
@@ -96,7 +102,7 @@ class BaseFlux:
         if width % 32 != 0 or height % 32 != 0:
             raise ValueError(f"Width {width} and height {height} must be multiples of 32.")
 
-    # NOTE: unused, see fluxdev class
+    # NOTE: unused,
     def generate_image(self, arguments):
         self.check_multiple_of_32(arguments["width"], arguments["height"])
 
@@ -115,35 +121,38 @@ class BaseFlux:
         # except Exception as e:
         #     print(f"Error generating image: {str(e)}")
         #     return self.create_blank_image()
-
         # todo: is unsued + needs vars updated
-        payload = {
-            "model": "flux-dev",
-            "prompt": arguments["prompt"],
-            "width": arguments["width"],
-            "height": arguments["height"],
-            "steps": arguments["steps"],
-            "hide_watermark": True,
-            "return_binary": False,
-            "seed": 123,
-            "cfg_scale": 123,
-            "style_preset": "Long Exposure",
-            # "negative_prompt": "<string>"
-        }
-        headers = {"Authorization": f"Bearer {os.getenv('VENICE_API_KEY')}", "Content-Type": "application/json"}
+        try:
+            payload = {
+                "model": arguments["model"],
+                "prompt": arguments["prompt"],
+                "negative_prompt": arguments["neg_prompt"],
+                "width": arguments["width"],
+                "height": arguments["height"],
+                "steps": arguments["steps"],
+                "hide_watermark": arguments["hide_watermark"],
+                "return_binary": False,
+                "seed": arguments["steps"],
+                "cfg_scale": arguments["cfg_scale"],
+                "style_preset": arguments["style_preset"],
+            }
+            headers = {"Authorization": f"Bearer {os.getenv('VENICE_API_KEY')}", "Content-Type": "application/json"}
 
-        base_url = os.getenv("VENICE_BASE_URL")
-        url = urljoin(base_url, API_ENDPOINTS["image_generate"])
-        response = requests.request("POST", url, json=payload, headers=headers)
-        return self.process_result(response.json())
+            url = urljoin(os.getenv("VENICE_BASE_URL"), API_ENDPOINTS["image_generate"])
+            response = requests.request("POST", url, json=payload, headers=headers)
+            return self.process_result(response.json())
+
+        except Exception as e:
+            print(f"Error generating image: {str(e)}")
+            return self.create_blank_image()
 
 
-class FluxDev(BaseFlux):
+class GenerateImage(GenerateImageBase):
     @classmethod
     def INPUT_TYPES(cls):
         return {
             "required": {  # todo: make api request to get model list instead of hardcoded
-                "model": (["flux-dev", "flux-dev-uncensored", "fluently-xl", "pony-realism"], {"default": "flux-dev"}),
+                "model": (super().get_models(type="image"), {"default": "flux-dev"}),
                 "prompt": ("STRING", {"default": "A flying cat made of lettuce", "multiline": True}),
                 "neg_prompt": (
                     "STRING",
@@ -183,6 +192,10 @@ class FluxDev(BaseFlux):
     def generate_image(
         self, model, prompt, neg_prompt, width, height, steps, guidance, style_preset, hide_watermark, seed=-1
     ):
+        if model in ["flux-dev", "flux-dev-uncensored"]:
+            print("Ignoring negative prompt for flux-dev and flux-dev-uncensored models")
+            neg_prompt = ""  # ignore negative to avoid error when using flux
+
         arguments = {  # todo: unused rn but used for when using base class generate img function
             "model": model,
             "prompt": prompt,
@@ -197,9 +210,6 @@ class FluxDev(BaseFlux):
         }
 
         try:
-            if model in ["flux-dev", "flux-dev-uncensored"]:
-                neg_prompt = ""  # ignore negative to avoid error when using flux
-
             # Override the base class method to handle the response directly
             self.check_multiple_of_32(width, height)
             seed = seed if seed != -1 else None  # i forgor
@@ -218,8 +228,7 @@ class FluxDev(BaseFlux):
             }
             headers = {"Authorization": f"Bearer {os.getenv('VENICE_API_KEY')}", "Content-Type": "application/json"}
 
-            base_url = os.getenv("VENICE_BASE_URL")
-            url = urljoin(base_url, API_ENDPOINTS["image_generate"])
+            url = urljoin(os.getenv("VENICE_BASE_URL"), API_ENDPOINTS["image_generate"])
             response = requests.request("POST", url, json=payload, headers=headers)
             return self.process_result(response.json())
 
@@ -298,7 +307,7 @@ class FluxDev(BaseFlux):
 # endregion
 
 NODE_CLASS_MAPPINGS = {
-    "GenerateImage_VENICE": FluxDev,  # todo: temp maybe
+    "GenerateImage_VENICE": GenerateImage,  # todo: temp maybe
     # "FluxPro_TOGETHER": FluxPro, # venice doesnt have flux pro/1.1/ultra
     # "FluxPro11_TOGETHER": FluxPro11,
 }
