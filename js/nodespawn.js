f52bcfcd183e9b11ddc94a6ab8401e44c377f2f5
Drac <altdragon@outlook.de>
2025-02-18 08:02:33 +0100

    remove defaulting from js

--- js/nodespawn.js
+++ js/nodespawn.js
@@ -27,9 +27,9 @@ app.registerExtension({
                         // Update widget options with image models only
                         modelWidget.options.values = data.image_models;
                         // Set the first value in data.text_models as the selected value
-                        if (data.image_models.length > 0) {
-                            modelWidget.value = data.image_models[0]; // Select the first model
-                        }
+                        // if (data.image_models.length > 0) {
+                        //     modelWidget.value = data.image_models[0]; // Select the first model
+                        // }
 
                         // Properly refresh the widget
                         if (modelWidget.onChange) {
@@ -63,9 +63,9 @@ app.registerExtension({
                         // Update widget styles
                         stylesWidget.options.values = data.data;
                         // Set the first value in data.text_models as the selected value
-                        if (data.data.length > 0) {
-                            stylesWidget.value = data.data[0]; // Select the first model
-                        }
+                        // if (data.data.length > 0) {
+                        //     stylesWidget.value = data.data[0]; // Select the first model
+                        // }
 
                         // Properly refresh the widget
                         if (stylesWidget.onChange) {
@@ -107,9 +107,9 @@ app.registerExtension({
                         // Update widget options with image models only
                         modelWidget.options.values = data.text_models;
                         // Set the first value in data.text_models as the selected value
-                        if (data.text_models.length > 0) {
-                            modelWidget.value = data.text_models[0]; // Select the first model
-                        }
+                        // if (data.text_models.length > 0) {
+                        //     modelWidget.value = data.text_models[0]; // Select the first model
+                        // }
 
                         // Properly refresh the widget
                         if (modelWidget.onChange) {
