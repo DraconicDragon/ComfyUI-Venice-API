49f73e7c0bef5be68b6f4815cf22a0d406e7af54
Drac <altdragon@outlook.de>
2025-06-15 01:05:58 +0200

    refactor: cleanup update_models.py a bit

--- pyserver/update_models.py
+++ pyserver/update_models.py
@@ -29,6 +29,17 @@ async def fetch_models_list():
 
         response_data = response.json()
 
+        # params = {"type": "embedding"}
+
+        # r2 = requests.get(url, headers=headers, params=params)
+        # r2.raise_for_status()  # Raises HTTPError for bad responses
+
+        # r2_data = r2.json()
+
+        # # concatenate response_data and r2_data
+        # if isinstance(response_data, list) and isinstance(r2_data, list):
+        #     response_data = response_data + r2_data
+
     # except requests.exceptions.HTTPError as http_err:
     #     print(f"HTTP error occurred: {http_err}")
     # except requests.exceptions.RequestException as req_err:
@@ -54,7 +65,8 @@ async def humanize_name(model_id: str) -> str:
     return humanized
 
 
-async def update_models_list():
+@routes.get("/veniceai/update_models_list")
+async def update_models_list_server(request):
     # img_model_json = await fetch_model_list("image")
     # txt_model_json = await fetch_model_list("text")
     # merged_json = {"object": "list", "data": img_model_json.get("data", []) + txt_model_json.get("data", [])}
@@ -103,16 +115,14 @@ async def update_models_list():
     enhanced_json = {"object": "list", "data": enhanced_data}
     with open(all_model_list_path, "w") as f:
         json.dump(enhanced_json, f, indent=2)
-    return ("model list update success", False)
 
-
-@routes.get("/veniceai/update_models_list")
-async def update_models_list_server(request):
-    response = await update_models_list()
+    response = ("Model list update success", False)
     return web.json_response({"message": response[0], "error": response[1]})
 
 
-async def get_local_models_list():
+# NOTE: routes are frozen and don't update with ComfyUI-HotReloadHack
+@routes.get("/veniceai/get_models_list")
+async def get_local_models_list_server(request):
     with open(all_model_list_path, "r") as f:
         model_list_json = json.load(f)
 
@@ -135,17 +145,11 @@ async def get_local_models_list():
     # if tts_voices:
     #     print(f"TTS Voices: {', '.join(tts_voices[:100])}")
 
-    return {
+    data = {
         "image_models": sorted(img_models),
         "text_models": sorted(txt_models),
         "tts_models": sorted(tts_models),
         "tts_voices": sorted(tts_voices),
         "model_list_json": model_list_json,
     }
-
-
-# NOTE: routes are frozen and don't update with ComfyUI-HotReloadHack
-@routes.get("/veniceai/get_models_list")
-async def get_local_models_list_server(request):
-    data = await get_local_models_list()
     return web.json_response(data)
