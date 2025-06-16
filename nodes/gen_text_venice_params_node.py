6a01db2161b162d47079ae4b0043e81a11a9dbb5
Drac <altdragon@outlook.de>
2025-06-16 18:53:55 +0200

    docs: add tooltips to textgen parameters node

--- nodes/gen_text_venice_params_node.py
+++ nodes/gen_text_venice_params_node.py
@@ -18,49 +18,72 @@ class GenerateTextVeniceParameters:
                     "COMBO",
                     {
                         "default": "strawberry-the-cat",
-                        "tooltip": "",
+                        "tooltip": ("The character slug of a public Venice character."),
                     },
                 ),
                 "enable_character": (
                     "BOOLEAN",
                     {
                         "default": False,
-                        "tooltip": "",
+                        "tooltip": ("Enable or disable character parameter being passed on."),
                     },
                 ),
                 "strip_thinking_response": (
                     "BOOLEAN",
                     {
                         "default": False,
-                        "tooltip": "",
+                        "tooltip": (
+                            "Strip <think></think> blocks from the response. "
+                            "Applicable only to reasoning / thinking models. "
+                            "Also available to use as a model feature suffix. "
+                            "Defaults to false."
+                        ),
                     },
                 ),
                 "disable_thinking": (
                     "BOOLEAN",
                     {
                         "default": False,
-                        "tooltip": "",
+                        "tooltip": (
+                            "On supported reasoning models, will disable thinking and "
+                            "strip the <think></think> blocks from the response. "
+                            "Defaults to false."
+                        ),
                     },
                 ),
                 "web_search": (
                     ["auto", "on", "off"],
                     {
                         "default": "auto",
-                        "tooltip": "",
+                        "tooltip": (
+                            "Auto will enable it based on the model's discretion. "
+                            "On will force web search on the request. "
+                            "Citations will be returned either in the first chunk of a "
+                            "streaming result, or in the non streaming response."
+                            "Defaults to off. "
+                        ),
                     },
                 ),
                 "enable_web_citations": (
                     "BOOLEAN",
                     {
                         "default": False,
-                        "tooltip": "",
+                        "tooltip": (
+                            "When web search is enabled, this will request that the LLM cite "
+                            "its sources using a [REF]0[/REF] format. "
+                            "Defaults to false."
+                        ),
                     },
-                ),
+                ),  # NOTE: include_search_results_in_stream not implemented
                 "use_venice_system_prompt": (
                     "BOOLEAN",
                     {
                         "default": False,
-                        "tooltip": "",
+                        "tooltip": (
+                            "Whether to include the Venice supplied system prompts "
+                            "alongside specified system prompts. "
+                            "Defaults to true."
+                        ),
                     },
                 ),
             },
@@ -70,6 +93,9 @@ class GenerateTextVeniceParameters:
     RETURN_NAMES = ("venice_parameters",)
     FUNCTION = "pass_on_params"
     CATEGORY = "venice.ai"
+    DESCRIPTION = (
+        "Passes on parameters unique parameters to Venice's API implementation for Venice text generation nodes."
+    )
 
     def pass_on_params(
         self,
